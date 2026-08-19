f79b3d287a33e5cb7bff78a2450f4458463c58c8
Emy-gold <imanelgarate@gmail.com>
2026-08-19 10:59:52 +0100

    add the project initial

---
+++ GestionStock.Shared/Entities/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionStock.Shared.Entities
+{
+    internal class ApplicationUser
+    {
+        public int Id { get; set;  }
+        public string Nom {  get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+        public string? Telephone { get; set; }
+
+        //Navigation
+        public ICollection<Operation> OperationsCreees { get; set; } = new List<Operation>();
+        public ICollection<Operation> OperationModifiees { get; set; } = new List<Operation>();
+
+    }
+}
---
+++ GestionStock.Shared/Entities/Operation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionStock.Shared.Entities
+{
+    internal class Operation
+    {
+    }
+}
