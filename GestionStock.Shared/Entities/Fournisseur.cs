46c5da78da6d3b8a37812a633126725c3e6be7c0
Emy-gold <imanelgarate@gmail.com>
2026-08-19 12:12:13 +0100

    add fournisseur entity

---
+++ GestionStock.Shared/Entities/Fournisseur.cs
@@ -0,0 +1,13 @@
+﻿namespace GestionStock.Shared.Entities;
+
+public class Fournisseur
+{
+    public int Id { get; set; }
+    public string Nom { get; set; } = string.Empty;
+    public string? Telephone { get; set; }
+    public string? Email { get; set; }
+    public string? Adresse { get; set; }
+    public bool Actif { get; set; } = true;
+
+    public ICollection<Operation> Operations { get; set; } = new List<Operation>();
+}
